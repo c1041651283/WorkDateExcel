86163f3a60c0c17f7992d80f8096f0a315e81627
chengguanwen <m18322003462@163.com>
2023-06-19 00:12:27 +0800

    创建项目结构

---
+++ ExcelCore/IAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelCore
+{
+    internal interface IAdapter
+    {
+        object GetOriginal();
+    }
+}
